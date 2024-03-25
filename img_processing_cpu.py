364c7f505b64ae7f720ae8ed9d6f15acce80794d
Efoks <edvardas.timoscenka@gmail.com>
2024-03-25 14:09:16 +0200

    Update the Gaussian noise function

--- img_processing_cpu.py
+++ img_processing_cpu.py
@@ -1,96 +1,70 @@
 import numpy as np
 from multiprocessing import Pool, cpu_count
+from threading import Thread
+from queue import Queue
 from skimage import io
 from src import config as cfg
 import matplotlib.pyplot as plt
 import os
 from src import image_processing_utils as f
 import time
+import logging
+from functools import reduce, partial
+from skimage.util import img_as_ubyte
 
 
 class ImageProcessor:
-    def __init__(self, img, thread_count = 12):
-        self.image = img
-        self.thread_count = thread_count
+    def __init__(self, workers):
+        self.cpu_workers = workers
+        self.pool = Pool(processes=workers)  # Pool is now instantiated once
+        self.black_pixels_count = None
 
     @staticmethod
-    def divide_image_into_strips(image, thread_count):
-        sub_img_height = image.shape[0] // thread_count
-        sub_images = [(image[i * sub_img_height:(i + 1) * sub_img_height, :]) for i in range(thread_count)]
-        if image.shape[0] % thread_count != 0:
-            sub_images.append(image[thread_count * sub_img_height:, :])
+    def divide_image_into_strips(image, cpu_workers):
+        sub_img_height = image.shape[0] // cpu_workers
+        sub_images = [(image[i * sub_img_height:(i + 1) * sub_img_height, :]) for i in range(cpu_workers)]
+        if image.shape[0] % cpu_workers != 0:
+            sub_images.append(image[cpu_workers * sub_img_height:, :])
         return sub_images
 
-    @staticmethod
-    def process_image(img, processing_function, thread_count):
-        sub_images = ImageProcessor.divide_image_into_strips(img, thread_count)
-        with Pool(processes=thread_count) as pool:
-            results = pool.map(processing_function, sub_images)
+    def process_image(self, img, processing_function):
+        sub_images = self.divide_image_into_strips(img, self.cpu_workers)
+        func_with_black_pixels_count = partial(processing_function, var=self.black_pixels_count)
+        results = self.pool.map(func_with_black_pixels_count, sub_images)
         return np.concatenate(results, axis=0)
 
-    def apply_gaussian_blur(self):
-        self.image = self.process_image(self.image, f.apply_gaussian_blur_to_sub_image, self.thread_count)
-        return self
+    def apply_gaussian_blur(self, img):
+        return self.process_image(img, f.apply_gaussian_blur_to_sub_image)
 
-    def apply_grayscale(self):
-        self.image = self.process_image(self.image, f.apply_conversion_to_grayscale, self.thread_count)
-        return self
+    def apply_grayscale(self, img):
+        grayscale_img = self.process_image(img, f.apply_conversion_to_grayscale)
+        self.black_pixels_count = np.sum(grayscale_img < 0.1)  # black pixel sum
+        return grayscale_img
 
-    def apply_gaussian_noise(self):
-        self.image = self.process_image(self.image, f.apply_gaussian_noise_to_sub_image, self.thread_count)
-        return self
+    def apply_gaussian_noise(self, img):
+        if self.black_pixels_count is None:
+            self.apply_grayscale(img)
+        noise_img = self.process_image(img, f.apply_gaussian_noise_to_sub_image)
+        self.black_pixels_count = None
+        return noise_img
 
-    def show_image(self):
-        plt.imshow(self.image, cmap='gray')
+    def show_image(self, img):
+        plt.imshow(img, cmap='gray')
         plt.title("Processed Image")
         plt.show()
 
-
-class BatchLoader:
-    def __init__(self, data_dir, batch_size, thread_count=12):
-
-        self.data_dir = data_dir
-        self.batch_size = batch_size
-        self.image_paths = self._load_image_paths()
-        self.current_index = 0
-        self.thread_count = thread_count
-
-    def _load_image_paths(self):
-        return [os.path.join(self.data_dir, image) for image in os.listdir(self.data_dir)]
-
-    def __len__(self):
-        return len(self.image_paths)
-
-    def __iter__(self):
-        self.current_index = 0
-        return self
-
-    def __next__(self):
-        if self.current_index >= len(self.image_paths):
-            raise StopIteration
-
-        start_idx = self.current_index
-        self.current_index += self.batch_size
-        end_idx = min(self.current_index, len(self.image_paths))
-        bathc_image_paths = self.image_paths[start_idx:end_idx]
-
-        with Pool(processes=self.thread_count) as pool:
-            batch_images = pool.map(io.imread, bathc_image_paths)
-
-        return batch_images
-
-    def __getitem__(self, idx):
-        return io.imread(self.image_paths[idx])
+    def close_pool(self):
+        self.pool.close()
 
 
 class ImageProcessingPipeline:
-    def __init__(self, data_dir, process_functions, batch_size = 4, thread_count=12):
-
-        data_loader_threads = int(thread_count * 0.25)
-        self.img_processing_threads = 12 - data_loader_threads
-
-        self.batch_loader = BatchLoader(data_dir, batch_size, data_loader_threads)
+    def __init__(self, data_dir, process_functions, batch_size, cpu_workers_count, results_dir=cfg.RESULTS_DIR):
+        self.data_dir = data_dir
         self.process_functions = process_functions
+        self.batch_size = batch_size
+        self.cpu_workers_count = cpu_workers_count
+        self.processor = ImageProcessor(cpu_workers_count)
+        self.results_dir = results_dir
 
         self.batch_times = []
         self.total_images_processed = 0
@@ -99,46 +73,65 @@ class ImageProcessingPipeline:
 
     def process_all_images(self):
         self.start_time = time.time()
+
+        images = os.listdir(self.data_dir)
+        print(f"Total images to process: {len(images)}")
         processed_images = []
 
-        for batch in self.batch_loader:
+        for i in range(0, len(images), self.batch_size):
             batch_start_time = time.time()
-            preprocessed_images = [ImageProcessor(img, self.img_processing_threads) for img in batch]
-            processed_images = [img.apply_gaussian_blur().image for img in preprocessed_images]
+            print(f"Processing batch {i // self.batch_size + 1}")
+
+            batch = images[i:i + self.batch_size]
+            batch_processed_images = self.process_batch(batch)
+            processed_images.extend(batch_processed_images)
+            print(f"Processed {len(batch_processed_images)} images")
+
             batch_end_time = time.time()
             self.batch_times.append(batch_end_time - batch_start_time)
-            self.total_images_processed += len(batch)
+            self.total_images_processed += len(batch_processed_images)
 
         self.end_time = time.time()
+
+        self.processor.close_pool()
         return processed_images
+
+    def process_batch(self, batch):
+        batch_processed_images = []
+        for img_name in batch:
+            img_path = os.path.join(self.data_dir, img_name)
+            img = io.imread(img_path)
+            processed_img = reduce(lambda x, func: self.processor.__getattribute__(func)(x), self.process_functions, img)
+            batch_processed_images.append(processed_img)
+            self.write_image(processed_img, img_name)
+            break
+
+        return batch_processed_images
+
+    def write_image(self, img, img_name):
+        result_path = os.path.join(self.results_dir, img_name)
+        if img.dtype != np.uint8:
+            img_to_save = img_as_ubyte(img)
+        else:
+            img_to_save = img
+        io.imsave(result_path, img_to_save)
 
     def display_metrics(self):
         total_time = self.end_time - self.start_time
-        print(f"Total execution time: {total_time} seconds")
-        if total_time > 0:
-            print(f"Images per second: {self.total_images_processed / total_time}")
-            print(f"Image processing techniques per second: {len(self.process_functions) * self.total_images_processed / total_time}")
-        for i, batch_time in enumerate(self.batch_times):
-            print(f"Time taken for batch {i+1}: {batch_time} seconds")
-
-def plot_images(images, rows = 2, cols = 2):
-    fig, axes = plt.subplots(rows, cols)
-    axes = axes.flatten()
-
-    for images, ax in zip(images, axes):
-        ax.imshow(images, cmap='gray')
-
-    plt.tight_layout()
-    plt.show()
+        print(f"Total execution time: {total_time:.2f} seconds")
+        print(f"Images per second: {self.total_images_processed / total_time:.2f}")
+        print(
+            f"Image processing techniques per second: {len(self.process_functions) * self.total_images_processed / total_time:.2f}")
+        print(f"Average time per batch: {np.mean(self.batch_times):.2f} seconds")
 
 
 if __name__ == '__main__':
-    images = os.listdir(cfg.DATA_DIR)
     pipeline = ImageProcessingPipeline(cfg.DATA_DIR,
-                                       [f.apply_gaussian_blur_to_sub_image,
-                                                       f.apply_conversion_to_grayscale,
-                                                       f.apply_gaussian_noise_to_sub_image]
+                                       ['apply_gaussian_blur', 'apply_grayscale', 'apply_gaussian_noise'],
+                                       batch_size=250,
+                                       cpu_workers_count=6
                                        )
+
     processed_images = pipeline.process_all_images()
     pipeline.display_metrics()
-    # plot_images(processed_images)
+
