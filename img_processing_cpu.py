5abe0320824eedab11417f68163d45f1f865f61f
Ugne <ugne.meskuotyte@gmail.com>
2024-03-25 19:29:10 +0200

    Added comments to the code

--- img_processing_cpu.py
+++ img_processing_cpu.py
@@ -14,51 +14,123 @@ from skimage.util import img_as_ubyte
 
 
 class ImageProcessor:
+    """
+    A class that processes image batches by dividing images into strips and applying transformations in parallel.
+    """
     def __init__(self, workers):
+        """
+        Initializes the ImageProcessor with a specific number of worker processes.
+        
+        Parameters:
+        workers (int): Number of worker processes to use for image processing tasks.
+        """
         self.cpu_workers = workers
         self.pool = Pool(processes=workers)  # Pool is now instantiated once
         self.black_pixels_count = None
 
-    @staticmethod
+    @staticmethod  
     def divide_image_into_strips(image, cpu_workers):
+        """
+        Divides an image into horizontal strips for parallel processing.
+        
+        :param image: The image to be divided.
+        :param cpu_workers: The number of workers, dictating the number of strips.
+        :return: A list of sub-images (strips).
+        """
+        #Determine height of each strip based on the number of workers
         sub_img_height = image.shape[0] // cpu_workers
+        #Create sub-images for each worker
         sub_images = [(image[i * sub_img_height:(i + 1) * sub_img_height, :]) for i in range(cpu_workers)]
+        #Handle any remainder of the image to ensure the entire image is covered
         if image.shape[0] % cpu_workers != 0:
             sub_images.append(image[cpu_workers * sub_img_height:, :])
         return sub_images
 
     def process_image(self, img, processing_function):
+        """
+        Processes an image by dividing it into strips and applying a given processing function in parallel.
+        
+        Parameters:
+        img (numpy.ndarray): The image to process.
+        processing_function (function): The function to apply to each strip.
+
+        Returns: 
+        img (numpy.ndarray): The processed image, reassembled from the processed strips.
+        """
         sub_images = self.divide_image_into_strips(img, self.cpu_workers)
         func_with_black_pixels_count = partial(processing_function, var=self.black_pixels_count)
+        #Process each strip in parallel.
         results = self.pool.map(func_with_black_pixels_count, sub_images)
+        #Reassemble the processed strips into a full image.
         return np.concatenate(results, axis=0)
 
     def apply_gaussian_blur(self, img):
+        """
+        Applies a Gaussian blur to an image.
+        
+        Parameters:
+        img (numpy.ndarray): The image to be blurred.
+        
+        Returns:
+        numpy.ndarray: The blurred image.
+        """
         return self.process_image(img, f.apply_gaussian_blur_to_sub_image)
 
     def apply_grayscale(self, img):
+        """
+        Convert the provided image to grayscale and count the number of black pixels.
+        
+        Parameters:
+        img (numpy.ndarray): The image to turn grayscale.
+        
+        Returns:
+        grayscale_img (numpy.ndarray): The grayscale version of the image.
+        """
         grayscale_img = self.process_image(img, f.apply_conversion_to_grayscale)
         self.black_pixels_count = np.sum(grayscale_img < 0.1)  # black pixel sum
         return grayscale_img
 
     def apply_gaussian_noise(self, img):
+        """
+        Applies Gaussian noise to an image. If the black pixels count hasn't been set, it applies
+        grayscale conversion first to determine that count.
+        
+        Parameters:
+        img (numpy.ndarray): The image to have noise added.
+        
+        Returns:
+        numpy.ndarray: The image with Gaussian noise added.
+        """
         if self.black_pixels_count is None:
             self.apply_grayscale(img)
         noise_img = self.process_image(img, f.apply_gaussian_noise_to_sub_image)
         self.black_pixels_count = None
         return noise_img
 
-    def show_image(self, img):
-        plt.imshow(img, cmap='gray')
-        plt.title("Processed Image")
-        plt.show()
-
     def close_pool(self):
+        """
+        Closes the processing pool.
+        """
         self.pool.close()
 
 
 class ImageProcessingPipeline:
+    """
+    A pipeline for processing images in batches using parallel processing, 
+    applying a series of specified image
+    processing functions to each image.
+    """
     def __init__(self, data_dir, process_functions, batch_size, cpu_workers_count, results_dir=cfg.RESULTS_DIR):
+        """
+        Initializes the image processing pipeline.
+        
+        Parameters:
+        data_dir (str): The directory containing the set of images to be processed.
+        process_functions (list of str): Names of the processing functions to be applied.
+        batch_size (int): The number of images to process in each batch.
+        cpu_workers_count (int): The number of worker processes to use for image processing.
+        results_dir (str): The directory where the processed images will be saved.
+        """
         self.data_dir = data_dir
         self.process_functions = process_functions
         self.batch_size = batch_size
@@ -72,31 +144,46 @@ class ImageProcessingPipeline:
         self.end_time = None
 
     def process_all_images(self):
+        """
+        Processes all images in the input directory, applying all specified functions.
+        It times the processing and prints out the total number of images to process.
+        """
+        #Save the time at the beginning of the processing
         self.start_time = time.time()
-
+        #Get a list of all the images in the directory
         images = os.listdir(self.data_dir)
         print(f"Total images to process: {len(images)}")
         processed_images = []
 
         for i in range(0, len(images), self.batch_size):
+            #Save the time at the beginning of batch processing
             batch_start_time = time.time()
             # print(f"Processing batch {i // self.batch_size + 1}")
-
             batch = images[i:i + self.batch_size]
             batch_processed_images = self.process_batch(batch)
             processed_images.extend(batch_processed_images)
             # print(f"Processed {len(batch_processed_images)} images")
 
+            #Save the time at the end of batch processing
             batch_end_time = time.time()
             self.batch_times.append(batch_end_time - batch_start_time)
             self.total_images_processed += len(batch_processed_images)
-
+        #Save the end time of the processing
         self.end_time = time.time()
 
         self.processor.close_pool()
         return processed_images
 
     def process_batch(self, batch):
+        """
+        Processes a batch of images, applying the specified processing functions to each image.
+        
+        Parameters:
+        batch (list of str): A list of image filenames to be processed.
+
+        Returns:
+        batch_processed_images (list of numpy.ndarray): A list of processed images.
+        """
         batch_processed_images = []
         for img_name in batch:
             img_path = os.path.join(self.data_dir, img_name)
@@ -104,19 +191,31 @@ class ImageProcessingPipeline:
             processed_img = reduce(lambda x, func: self.processor.__getattribute__(func)(x), self.process_functions, img)
             batch_processed_images.append(processed_img)
             self.write_image(processed_img, img_name)
-            break
 
         return batch_processed_images
 
     def write_image(self, img, img_name):
+        """
+        Save the processed image into the specified directory.
+        
+        Parameters:
+        img (numpy.ndarray): The processed image to be saved.
+        img_name (str): The name of the file for the saved image.
+        """
+        #Get a file path for the output image
         result_path = os.path.join(self.results_dir, img_name)
         if img.dtype != np.uint8:
             img_to_save = img_as_ubyte(img)
         else:
             img_to_save = img
+        #Save the image into the directory
         io.imsave(result_path, img_to_save)
 
     def display_metrics(self):
+        """
+        Display processing time metrics after all images have been processed.
+        """
+        #Calculate the amount of time it took to process all of the images
         total_time = self.end_time - self.start_time
         print(f"Total execution time: {total_time:.2f} seconds")
         print(f"Images per second: {self.total_images_processed / total_time:.2f}")
@@ -126,12 +225,13 @@ class ImageProcessingPipeline:
 
 
 if __name__ == '__main__':
+    #Create an instance of the class ImageProcessor:
     pipeline = ImageProcessingPipeline(cfg.DATA_DIR,
                                        ['apply_gaussian_blur', 'apply_grayscale', 'apply_gaussian_noise'],
-                                       batch_size=250,
-                                       cpu_workers_count=6
+                                       batch_size=25,
+                                       cpu_workers_count=2
                                        )
-
+    #Calls upon the processor functions
     processed_images = pipeline.process_all_images()
     pipeline.display_metrics()
 
--- img_processing_sequantial.py
+++ img_processing_sequantial.py
@@ -1,4 +1,4 @@
-import numpy as np
+import numpy as np 
 from skimage import io
 import os
 from src import config as cfg
@@ -8,57 +8,137 @@ from skimage.util import img_as_ubyte
 
 
 class SequentialImageProcessor:
+    """
+    A class that processes images sequentially using a set of provided image processing functions.
+    This processor reads images from a data folder, applies each function, and saves the images into the results folder on disc.
+    """
     def __init__(self, data_dir, process_functions, results_dir=cfg.RESULTS_DIR):
+        """
+        Initialization of the class SequentialImageProcessor.
+        
+        Parameters:
+        data_dir (str): The directory containing the set of images to be processed.
+        process_functions (list of str): Names of the processing functions to be applied.
+        results_dir (str): The directory where the processed images will be saved.
+        """
+
+        #Path of the input image directory
         self.data_dir = data_dir
+        #List of processing functions to apply
         self.process_functions = process_functions
+        #Path to the output image directory
         self.results_dir = results_dir
 
+        #Variables to keep track of the number of processed images and times
         self.total_images_processed = 0
         self.start_time = None
         self.end_time = None
 
+        #Variable to keep track of black pixels in an image
         self.black_pixels_count = None
 
     def process_image(self, img, processing_function):
+        """
+        Processes an image using a specified function.
+        
+        Parameters:
+        img (numpy.ndarray): The image to be processed.
+        processing_function (function): The function to apply to the image.
+        
+        Returns:
+        numpy.ndarray: The processed image.
+        """
+        # Call the processing function with the image and the black pixel count variable
         return processing_function(img, self.black_pixels_count)
 
     def apply_gaussian_blur(self, img):
+        """
+        Applies a Gaussian blur to an image.
+        
+        Parameters:
+        img (numpy.ndarray): The image to be blurred.
+        
+        Returns:
+        numpy.ndarray: The blurred image.
+        """
         return self.process_image(img, f.apply_gaussian_blur_to_sub_image)
 
     def apply_grayscale(self, img):
-        grayscale_img = self.process_image(img, f.apply_conversion_to_grayscale)
-        self.black_pixels_count = np.sum(grayscale_img < 0.1)  # black pixel sum
+        """
+        Convert the provided image to grayscale and count the number of black pixels.
+        
+        Parameters:
+        img (numpy.ndarray): The image to turn grayscale.
+        
+        Returns:
+        grayscale_img (numpy.ndarray): The grayscale version of the image.
+        """
+        grayscale_img = self.process_image(img, f.apply_conversion_to_grayscale) #Logic of this roundabout function calling should be improved
+        #Counts the number of pixels that reach the "black" benchmark
+        self.black_pixels_count = np.sum(grayscale_img < 0.1)  #Black pixel sum
         return grayscale_img
 
     def apply_gaussian_noise(self, img):
+        """
+        Applies Gaussian noise to an image. If the black pixels count hasn't been set, it applies
+        grayscale conversion first to determine that count.
+        
+        Parameters:
+        img (numpy.ndarray): The image to have noise added.
+        
+        Returns:
+        numpy.ndarray: The image with Gaussian noise added.
+        """
         if self.black_pixels_count is None:
             self.apply_grayscale(img)
         noise_img = self.process_image(img, f.apply_gaussian_noise_to_sub_image)
-        self.black_pixels_count = None
+        self.black_pixels_count = None #Reset the black pixel count for the next image to be processed
         return noise_img
 
     def process_all_images(self):
+        """
+        Processes all images in the input directory, applying all specified functions.
+        It times the processing and prints out the total number of images to process.
+        """
+        #Save the time at the beginning of the processing
         self.start_time = time.time()
+        #Get a list of all the images in the directory
         images = os.listdir(self.data_dir)
         print(f"Total images to process: {len(images)}")
         for img_name in images:
-            img_path = os.path.join(self.data_dir, img_name)
-            img = io.imread(img_path)
+            img_path = os.path.join(self.data_dir, img_name) #Get the path to the image being processed
+            img = io.imread(img_path) #COnvert the image into numpy ndarray
+            #Apply all the given functions to the image
             for func in self.process_functions:
                 img = self.__getattribute__(func)(img)
+            #Save the processed image
             self.write_image(img, img_name)
             self.total_images_processed += 1
+        #Save the end time of the processing
         self.end_time = time.time()
 
     def write_image(self, img, img_name):
-        result_path = os.path.join(self.results_dir, img_name)
+        """
+        Save the processed image into the specified directory.
+        
+        Parameters:
+        img (numpy.ndarray): The processed image to be saved.
+        img_name (str): The name of the file for the saved image.
+        """
+        #Get a file path for the output image
+        result_path = os.path.join(self.results_dir, img_name) 
         if img.dtype != np.uint8:
             img_to_save = img_as_ubyte(img)
         else:
             img_to_save = img
+        #Save the image into the directory
         io.imsave(result_path, img_to_save)
 
     def display_metrics(self):
+        """
+        Display processing time metrics after all images have been processed.
+        """
+        #Calculate the amount of time it took to process all of the images
         total_time = self.end_time - self.start_time
         print(f"Total execution time: {total_time:.2f} seconds")
         print(f"Images per second: {self.total_images_processed / total_time:.2f}")
@@ -66,9 +146,11 @@ class SequentialImageProcessor:
             f"Image processing techniques per second: {len(self.process_functions) * self.total_images_processed / total_time:.2f}")
 
 if __name__ == '__main__':
+    #Create an instance of the SequentialImageProcessor
     processor = SequentialImageProcessor(cfg.DATA_DIR,
                                          ['apply_gaussian_blur',
                                           'apply_grayscale',
                                           'apply_gaussian_noise'])
-
+    #Calls upon the processor functions
     processor.process_all_images()
+    processor.display_metrics()
