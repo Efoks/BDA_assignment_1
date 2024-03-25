dd53a381784fdea359ace4513b7feb6775309c5d
Efoks <edvardas.timoscenka@gmail.com>
2024-03-25 14:21:59 +0200

    Small changes in src folder

--- src/config.py
+++ src/config.py
@@ -3,3 +3,21 @@ import os
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATA_DIR = os.path.join(PROJECT_DIR, 'data')
 
+LOG_DIR = os.path.join(PROJECT_DIR, 'logs')
+if not os.path.exists(LOG_DIR):
+    os.makedirs(LOG_DIR)
+
+LOG_DIR_SEQUENTIAL = os.path.join(LOG_DIR, 'sequential')
+if not os.path.exists(LOG_DIR_SEQUENTIAL):
+    os.makedirs(LOG_DIR_SEQUENTIAL)
+LOG_DIR_CPU = os.path.join(LOG_DIR, 'cpu')
+if not os.path.exists(LOG_DIR_CPU):
+    os.makedirs(LOG_DIR_CPU)
+LOG_DIR_GPU = os.path.join(LOG_DIR, 'gpu')
+if not os.path.exists(LOG_DIR_GPU):
+    os.makedirs(LOG_DIR_GPU)
+
+RESULTS_DIR = os.path.join(PROJECT_DIR, 'results')
+if not os.path.exists(RESULTS_DIR):
+    os.makedirs(RESULTS_DIR)
+
--- src/image_processing_utils.py
+++ src/image_processing_utils.py
@@ -1,14 +1,19 @@
 from skimage import io, filters, util
 import numpy as np
 
-def apply_gaussian_blur_to_sub_image(sub_image):
-    result = filters.gaussian(sub_image, sigma=2, channel_axis=-1)
+
+# Var is a dummy argument used only in noise function
+def apply_gaussian_blur_to_sub_image(sub_image, var = None):
+    result = filters.gaussian(sub_image, sigma=1, channel_axis=-1)
     return result
 
-def apply_conversion_to_grayscale(sub_image):
+def apply_conversion_to_grayscale(sub_image, var = None):
     grayscale_sub_image = np.dot(sub_image[..., :3], [0.299, 0.587, 0.114])
     return grayscale_sub_image
 
-def apply_gaussian_noise_to_sub_image(sub_image):
-    result = util.random_noise(sub_image, mode='gaussian', var=0.01)
+def apply_gaussian_noise_to_sub_image(sub_image, var):
+    base_variance = 0.00001
+    var_prop = var / np.prod(sub_image.shape)
+    variance = base_variance + (var_prop * base_variance)
+    result = util.random_noise(sub_image, mode='gaussian', var=variance)
     return result
